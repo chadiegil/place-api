e2de23c794a081f8e0b15de0ac2bb201c75cfbab
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-03-18 08:02:22 +0800

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "secret",
+  database: "place_api",
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
+});
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM place";
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
+app.get("/place/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM place WHERE id=${id}`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result[0]);
+  });
+});
+
+app.post("/place", (req, res) => {
+  const { name, email, phone } = req.body;
+  const sql = `INSERT INTO place (name, email, phone) VALUES ('${name}', '${email}', '${phone}')`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
+app.put("/place/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  const sql = `UPDATE place SET name='${name}', email='${email}', phone='${phone}' WHERE id=${id}`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
+app.delete("/place/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM place WHERE id=${id}`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
